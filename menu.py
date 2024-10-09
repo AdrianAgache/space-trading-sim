c9252e40a2fb500c0ffcdec5d706733ddd75049b
TrooperXZ <22agache@parmiters.herts.sch.uk>
2024-10-09 14:06:18 +0100

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,22 @@
+from time import sleep
+from os import *
+ans = True
+while ans == True:
+    print("<SHIP LOADING>")
+    sleep(0.5)
+    for i in range(0, 101):
+      system("cls")
+      print("<SHIP LOADING: ", i, "%>")
+      sleep(0.5)
+    print("<Welcome Captain>")
+    ans=input("What would you like to do?\n")
+    if ans=="1": 
+      print("") 
+    elif ans=="2":
+      print("") 
+    elif ans=="3":
+      print("") 
+    elif ans=="4":
+      print("") 
+    else:
+      print("")
